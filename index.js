ed4c706b270eb3f72096526f5214c6c7d9f0bb7e
Ahmedmustaffa <89359266+Ahmedmustaffa@users.noreply.github.com>
2024-12-22 15:12:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ app.use(express.json()); // Middleware for parsing JSON requests
 // Connect to MongoDB
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log('Error connecting to MongoDB:', err));
-
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Error connecting to MongoDB:', err));
 
 // Test route
 app.get('/', (req, res) => {
-    res.send('Welcome to the ASD Social Website!');
+  res.send('Welcome to the ASD Social Website!');
 });
 
-// Start the server
-app.listen(3000, () => {
-    console.log(`Server is running`);
-});
+// Start the server (Vercel will handle this for you in its environment)
+module.exports = app; // Export the app for Vercel
